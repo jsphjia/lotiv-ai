9fcf6b94892deb0ba256bcbed8dbe63b614939f1
jsphjia <joseph.jia23@gmail.com>
2025-12-30 15:27:12 -0500

    updated organization, plot code, env setup

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from generate_land_plot import plot_coordinates
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/bounding-box', methods=['POST'])
+def receive_bounding_box():
+    data = request.get_json()
+    if data:
+        print("Received Shape Data:", data, flush=True)  # Print all shape data
+        if "rings" in data:  # Check if the shape contains rings (polygon data)
+            for ring in data["rings"]:
+                print("Ring Coordinates:", ring, flush=True)
+                plot_coordinates(ring)  # Plot the coordinates immediately
+        return jsonify({"message": "Shape data received and plotted successfully."}), 200
+    else:
+        return jsonify({"message": "No data received."}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/generate_land_plot.py
+++ backend/generate_land_plot.py
@@ -1,12 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 import matplotlib
-
 matplotlib.use('Agg')  # Use a non-interactive backend
 import matplotlib.pyplot as plt
-
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
 def normalize_coordinates(coordinates):
     # Use the first coordinate as the origin (0, 0)
@@ -38,19 +32,3 @@ def plot_coordinates(coordinates):
     # Save the plot as an image
     plt.savefig('plot.png', bbox_inches='tight', pad_inches=0)
     plt.close(fig)  # Close the figure to free resources
-
-@app.route('/bounding-box', methods=['POST'])
-def receive_bounding_box():
-    data = request.get_json()
-    if data:
-        print("Received Shape Data:", data, flush=True)  # Print all shape data
-        if "rings" in data:  # Check if the shape contains rings (polygon data)
-            for ring in data["rings"]:
-                print("Ring Coordinates:", ring, flush=True)
-                plot_coordinates(ring)  # Plot the coordinates immediately
-        return jsonify({"message": "Shape data received and plotted successfully."}), 200
-    else:
-        return jsonify({"message": "No data received."}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
